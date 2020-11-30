42c6594e846e09ca889d114e7c9a52876ca40156
SaritMeshesha <55807798+SaritMeshesha@users.noreply.github.com>
2020-11-30 14:30:43 +0200

    Header component added

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,10 +1,25 @@
+import { HeaderStyleInterpolators } from '@react-navigation/stack';
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Header() {
     return (
-        <View>
-            <Text>Header</Text>
+        <View style={styles.header}>
+            <Text style={styles.title}>תהיי בריאה Logo</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        paddingTop: 30,
+        backgroundColor: '#E6E6FA'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#000',
+    }
+});
--- client/screens/Welcome.js
+++ client/screens/Welcome.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from '../components/Header';
 
 export default function Welcome() {
     return (
         <View>
-            <Text>Welcome</Text>
+            <View>
+                <Header />
+            </View>
+            <View>
+                <Text>Welcome</Text>
+            </View>
         </View>
     )
 }
