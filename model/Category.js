fce69f0e5822df5a6b0d02cc6e0f59b7babbc3bd
aschalegn <aschaley@gmail.com>
2020-12-02 11:45:06 +0200

    update form

---
+++ model/Category.js
@@ -0,0 +1,10 @@
+const { Mongoose } = require('mongoose');
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+
+});
+
+const Category = mongoose.model("category")
